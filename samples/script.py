6a1114b51a6ce32e850f6f8d0a9e7e44a2c14088
setarejahandide <se.jahani@gmail.com>
2025-12-09 11:58:53 +0100

    add mizan samples

--- samples/script.py
+++ samples/script.py
@@ -1,7 +1,7 @@
 # script/create_sample
 
-input_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\MIZAN-FA\mizan_words and sentences.xml"
-output_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\github_corpora\samples\mizan_sample.xml"
+input_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\MIZAN-FA\fa (1).txt"
+output_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\github_corpora\samples\mizan_sample_raw.xml"
 
 num_lines = 50   # size of sample
 
