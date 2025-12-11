34d1c78d3dab5ba858d718476c957e759e43da99
setarejahandide <se.jahani@gmail.com>
2025-12-11 16:01:00 +0100

    adding Leipzig sample

--- samples/script.py
+++ samples/script.py
@@ -1,9 +1,9 @@
 # script/create_sample
 
-input_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\MIZAN-FA\fa (1).txt"
-output_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\github_corpora\samples\mizan_sample_raw.xml"
+input_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\Leipzig corpus\fas_news_2016_300K\fas_news_2016_300K-sentences.txt"
+output_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\github_corpora\samples\Leipzig_sample_sentences.xml"
 
-num_lines = 50   # size of sample
+num_lines = 2000   # size of sample
 
 with open(input_file, "r", encoding="utf-8") as fin, \
      open(output_file, "w", encoding="utf-8") as fout:
