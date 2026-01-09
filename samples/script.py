5470e6cbbb5609f34992505205fa240c14484d7c
setarejahandide <se.jahani@gmail.com>
2026-01-09 13:27:48 +0100

    Matina sample

--- samples/script.py
+++ samples/script.py
@@ -1,9 +1,9 @@
 # script/create_sample
 
-input_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\Leipzig corpus\fas_news_2016_300K\fas_news_2016_300K-sentences.txt"
-output_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\github_corpora\samples\Leipzig_sample_sentences.xml"
+input_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\Matina\Matina-D1403032901_newsKhordad1403_dedup90_P.jsonl"
+output_file = r"C:\Users\User\Documents\germany\thesis\semantic change\database\Matina\Matina-newsKhordad1403.jsonl"
 
-num_lines = 2000   # size of sample
+num_lines = 50   # size of sample
 
 with open(input_file, "r", encoding="utf-8") as fin, \
      open(output_file, "w", encoding="utf-8") as fout:
